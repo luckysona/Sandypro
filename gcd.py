8095dc7d0f4dbd1b457f72ae9ffbe1cd0940e5c7
luckysona <50876072+luckysona@users.noreply.github.com>
2019-06-18 05:27:41 -0700

    Update gcd.py

--- gcd.py
+++ gcd.py
@@ -1,8 +1,8 @@
 import math
 import functools
-inp11,inp21=map(int,input().split())
-List=[int(i) for i in input().split()]
-for i in range(inp21):
-    c,d=map(int,input().split())
-    temp=functools.reduce(math.gcd,List[c-1:d])
+inp11,inp31=map(int,input().split())
+List=[int(j) for j in input().split()]
+for j in range(inp31):
+    n,k=map(int,input().split())
+    temp=functools.reduce(math.gcd,List[n-1:k])
     print(temp)
