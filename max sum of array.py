54f965335eb723ee03f21a02c7b171ec61906832
luckysona <50876072+luckysona@users.noreply.github.com>
2019-06-20 06:45:22 -0700

    Update max sum of array.py

--- max sum of array.py
+++ max sum of array.py
@@ -1,8 +1,11 @@
-s1=int(input())
-n2=list(map(int,input().split()))
+n=int(input())
+s=list(map(int,input().split()))
 L=[]
-d=max(n2)  
-x=n2.remove(d)  
-s=max(n2)  
-e=d+s    
-print(e)
+d=0
+for i in range(0,n-2):
+	for j in range(i,n,2):
+		d=d+s[j]
+	L.append(d)
+	d=0
+L.sort()
+print(L[-1])
