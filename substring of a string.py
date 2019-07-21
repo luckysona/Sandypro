30f9c35ba7e0742400ce4fb9bd50a9c03f1cfe4a
luckysona <50876072+luckysona@users.noreply.github.com>
2019-07-21 06:02:05 -0700

    Update substring of a string.py

--- substring of a string.py
+++ substring of a string.py
@@ -1,5 +1,7 @@
-str1,str2=input().split()    
-if(str1.find(str2)==-1):
-  print("yes")
-else:
-  print("no")
+def check(str1,str2): 
+	if (str1.find(str2) == -1): 
+		print("no") 
+	else: 
+		print("yes") 
+str1,str2=input().split()
+check(str1,str2) 
