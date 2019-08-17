cb191111a74359181989d1e940048e697e6db6aa
luckysona <50876072+luckysona@users.noreply.github.com>
2019-08-17 01:34:45 -0700

    Update printing smallest num after deleting k digits.py

--- printing smallest num after deleting k digits.py
+++ printing smallest num after deleting k digits.py
@@ -1,7 +1,7 @@
 from itertools import combinations
-num1,num2=map(int,input().split())
-g=len(str(num1))
-H=list(combinations(str(num1),g-num2))
+d1,d2=map(int,input().split())
+g=len(str(d1))
+H=list(combinations(str(d1),g-d2))
 H=(sorted(H))
 l="".join(H[0])
 print(l)
